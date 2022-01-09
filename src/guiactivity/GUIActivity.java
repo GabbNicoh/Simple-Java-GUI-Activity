668d5bda4e92f3462e4931deb949541707afbb7e
sawcon <gabbnicoh@gmail.com>
2022-01-09 10:17:51 +0800

    standard stuff

---
+++ src/guiactivity/GUIActivity.java
@@ -0,0 +1,11 @@
+
+package guiactivity;
+
+public class GUIActivity {
+
+    public static void main(String[] args) {
+        SimpleCalculator cal = new SimpleCalculator();
+        cal.setVisible(true);
+    }
+    
+}
